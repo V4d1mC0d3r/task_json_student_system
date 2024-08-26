902ad47e662d56e9234931069bbc2488d99e2407
V4d1mC0d3r <vadimgordikov78@gmail.com>
2024-08-26 13:07:28 +0600

    created State class

--- src/states/State.java
+++ src/states/State.java
@@ -1,5 +1,22 @@
 package states;
 
-public abstract class State {
+import main_app.Student;
+import main_app.StudentDatabase;
 
+public abstract class State {
+    protected StudentDatabase studentDatabase;
+
+    public State(StudentDatabase studentDatabase) {
+        this.studentDatabase = studentDatabase;
+    }
+
+    public abstract void addStudent(Student student);
+
+    public abstract void removeStudent(int id);
+
+    public abstract void findStudent(int id);
+
+    public abstract void saveToFile(Student filename);
+
+    public abstract void loadFromFile(Student filename);
 }
